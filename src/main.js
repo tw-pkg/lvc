b1519f03def1843c1a52ed109972d64e41867da0
nswon <swnam0729@gmail.com>
2024-04-28 16:57:52 +0900

    react + electron

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
