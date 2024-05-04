0d47614d7eba56d7e8300b07975f15e5d32bf74d
Luffy-blaybus <156280160+Luffy-blaybus@users.noreply.github.com>
2024-05-04 16:05:42 +0900

    .

--- src/main/main.js
+++ src/main/main.js
@@ -6,8 +6,8 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1400,
+    height: 850,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
