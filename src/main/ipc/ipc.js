30ff9c9b5ee44cd1201e52c7c1c2ba2a7443d884
nswon <swnam0729@gmail.com>
2024-05-12 01:22:55 +0900

    fetch history 수정

--- src/main/ipc/ipc.js
+++ src/main/ipc/ipc.js
@@ -4,7 +4,7 @@ const { Credentials } = require('../credentials')
 
 function registerGlobalListeners() {
   ipcMain.handle('summoner-history', async (_, puuid) => {
-    const data = Credentials.request(`/lol-match-history/v1/products/lol/${puuid}/matches`, 'GET')
+    const data = await Credentials.request(`/lol-match-history/v1/products/lol/${puuid}/matches`, 'GET')
     const history = new History(data);
     return history.getStats();
   });
