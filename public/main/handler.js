2f9bba493702dc0a17a04464573d37082dfe2525
nswon <swnam0729@gmail.com>
2024-05-18 13:05:13 +0900

    handler 제거

--- public/main/handler.js
+++
@@ -1,9 +0,0 @@
-function joinTeamVoice(team) {
-  console.log(team[0]);
-  const { profileIconId, puuid, summonerId, summonerName } = team[0];
-  console.log(profileIconId, puuid, summonerId, summonerName);
-}
-
-module.exports = {
-  joinTeamVoice
-}
